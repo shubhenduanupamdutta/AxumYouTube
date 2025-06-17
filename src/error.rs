f841a19082282d18feac74209b8510e4d2f7f4d6
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-17 16:22:15 +0530

    Implementing IntoResponse Trait

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,24 @@
+use axum::{http::StatusCode, response::IntoResponse};
+
 #[derive(Debug, thiserror::Error)]
 pub enum ApiError {
     #[error("Details: {0}")]
     InternalServerError(String),
+    #[error("Login Failed")]
+    LoginFail,
 }
 
 impl ApiError {
-    pub fn new(message: String) -> Self {
+    pub(crate) fn new(message: String) -> Self {
         ApiError::InternalServerError(message)
+    }
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> axum::response::Response {
+        println!("->> {:<12} - {self:?}", "INTO_RES");
+
+        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
     }
 }
 
