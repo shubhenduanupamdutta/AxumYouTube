3d2e3282336a4b7eb53d39157bec7670fae810f7
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-17 13:54:14 +0530

    Using a function as handler

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use axum_youtube_code_along::error::MainError;
 
 #[tokio::main]
 async fn main() -> Result<(), MainError> {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(hello_handler));
 
-    // region   --- start server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> Listening on {addr}\n");
     let listener = tokio::net::TcpListener::bind(addr)
         .await
         .map_err(|err| MainError::new(err.to_string()))?;
+
     axum::serve(listener, routes_hello)
         .await
         .map_err(|err| MainError::new(err.to_string()))?;
-    // endregion --- start server
 
     Ok(())
+}
+
+async fn hello_handler() -> impl IntoResponse {
+    println!("->> {:<12} - hello_handler", "HANDLER");
+
+    Html("Hello <strong>World!!!</strong>")
 }
