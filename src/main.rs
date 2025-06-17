04ec1839bd0086704f39dc9ecd35204e31279f3e
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-17 14:31:41 +0530

    A directory as fallback

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use std::net::SocketAddr;
 use axum::{
     extract::{Path, Query},
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, get_service},
     Router,
 };
 use axum_youtube_code_along::error::MainError;
 use serde::{Deserialize, Serialize};
+use tower_http::services::ServeDir;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct HelloParams {
@@ -16,9 +17,9 @@ struct HelloParams {
 
 #[tokio::main]
 async fn main() -> Result<(), MainError> {
-    let routes_hello = Router::new()
-        .route("/hello", get(hello_handler))
-        .route("/hello2/{name}", get(hello_handler_2));
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> Listening on {addr}\n");
@@ -26,11 +27,17 @@ async fn main() -> Result<(), MainError> {
         .await
         .map_err(|err| MainError::new(err.to_string()))?;
 
-    axum::serve(listener, routes_hello)
+    axum::serve(listener, routes_all)
         .await
         .map_err(|err| MainError::new(err.to_string()))?;
 
     Ok(())
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(hello_handler))
+        .route("/hello2/{name}", get(hello_handler_2))
 }
 
 async fn hello_handler(Query(q_params): Query<HelloParams>) -> impl IntoResponse {
@@ -43,4 +50,8 @@ async fn hello_handler(Query(q_params): Query<HelloParams>) -> impl IntoResponse
 async fn hello_handler_2(Path(name): Path<String>) -> impl IntoResponse {
     println!("->> {:<12} - hello_handler - {name:?}", "HANDLER");
     Html(format!("Hello <strong>{name}</strong>"))
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
