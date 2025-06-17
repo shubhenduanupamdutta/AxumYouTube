5d945ef682f10dbf7ebbf26c04d78a06eb7b1cdb
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-17 13:47:58 +0530

    A simple test for hello route

---
+++ tests/quick_dev.rs
@@ -0,0 +1,10 @@
+use anyhow::Result;
+
+const ENDPOINT_URL: &str = "http://127.0.0.1:8080";
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client(ENDPOINT_URL)?;
+    hc.do_get("/hello").await?.print().await?;
+    Ok(())
+}
