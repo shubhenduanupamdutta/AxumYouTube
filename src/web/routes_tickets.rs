47decaca818b37fe964b16583348085efa86a3c5
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-19 14:41:30 +0530

    Securing routes by adding ctx

--- src/web/routes_tickets.rs
+++ src/web/routes_tickets.rs
@@ -2,33 +2,36 @@ use axum::extract::{Path, State};
 use axum::routing::{delete, post};
 use axum::{Json, Router};
 
+use crate::ctx::Ctx;
 use crate::model::{ModelController, Ticket, TicketCreate};
 use crate::Result;
 
 async fn create_ticket(
+    ctx: Ctx,
     State(mc): State<ModelController>,
     Json(ticket_fc): Json<TicketCreate>,
 ) -> Result<Json<Ticket>> {
     println!("->> {:<12} - create_ticket", "HANDLER");
 
-    let ticket = mc.create_ticket(ticket_fc).await?;
+    let ticket = mc.create_ticket(ctx, ticket_fc).await?;
 
     Ok(Json(ticket))
 }
 
-async fn list_tickets(State(mc): State<ModelController>) -> Result<Json<Vec<Ticket>>> {
+async fn list_tickets(ctx: Ctx, State(mc): State<ModelController>) -> Result<Json<Vec<Ticket>>> {
     println!("->> {:<12} - list_tickets", "HANDLER");
 
-    let tickets = mc.list_ticket().await?;
+    let tickets = mc.list_ticket(ctx).await?;
     Ok(Json(tickets))
 }
 
 async fn delete_ticket(
+    ctx: Ctx,
     State(mc): State<ModelController>,
     Path(id): Path<u64>,
 ) -> Result<Json<Ticket>> {
     println!("->> {:<12} - delete_ticket", "HANDLER");
-    let ticket = mc.delete_ticket(id).await?;
+    let ticket = mc.delete_ticket(ctx, id).await?;
     Ok(Json(ticket))
 }
 
